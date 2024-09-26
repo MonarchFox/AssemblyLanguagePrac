8e1dc62ecbead817ea37ca42ca376a7b8a300ce0
MonarchFox <165113287+MonarchFox@users.noreply.github.com>
2024-09-26 23:19:42 +0530

    Update AbbreviateTwoWordName.asm

--- Codewar/AbbreviateTwoWordName.asm
+++ Codewar/AbbreviateTwoWordName.asm
@@ -1,21 +1,34 @@
 section .text
+
 global abbrev_name
 
 abbrev_name:
-  movzx rax, byte [rdi]   
-  mov [rsi], al           
+  movzx rax, byte [rdi]
+  call to_uppercase
+  mov [rsi], al
   mov byte [rsi + 1], '.'
-  mov rcx, 0              
+
+  mov rcx, 0
 
 find_space:
-  inc rcx                 
-  mov bl, [rdi + rcx]     
-  cmp bl, 32              
-  jne find_space         
+  inc rcx
+  mov bl, [rdi + rcx]
+  cmp bl, 32
+  jne find_space
   
-  inc rcx                 
-  movzx rax, byte [rdi + rcx] 
-  mov [rsi + 2], al      
-  mov byte [rsi + 3], 0 
+  inc rcx
+  movzx rax, byte [rdi + rcx]
+  call to_uppercase
+  mov [rsi + 2], al
 
-  ret                    
+  mov byte [rsi + 3], 0
+  ret
+
+to_uppercase:
+  cmp al, 'a'
+  jl  end_uppercase
+  cmp al, 'z'
+  jg  end_uppercase
+  sub al, 32
+end_uppercase:
+  ret
